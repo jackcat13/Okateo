b9a757d5462bab734dd643654518e62f89808307
Quentin Choulet <choulet.quentin@gmail.com>
2016-01-26 11:04:34 +0100

    Middleware now expose a webservice to write on serial port

--- middleware/nodejs_serial/main.js
+++ middleware/nodejs_serial/main.js
@@ -1,7 +1,12 @@
+const DEBUG = true;
+
 var readline = require('readline');
 var SerialPort = require('serialport').SerialPort;
+var http = require('http');
+var url = require('url');
 
-var serialPort, rl, port;
+var serialPort, rl, port, httpServer;
+var httpPort = 8008;
 
 init();
 
@@ -19,7 +24,25 @@ function init (){
 			output: process.stdout
 		});
 		rl.on('line', onStdinLine);
+
+		httpServer = http.createServer(requestHandler);
+		httpServer.listen(httpPort, 'localhost', null, onHttpServerStarted);
+
+		console.log('Application is starting ...\n');
+		console.log('Type \'stop\' to terminate the process\n');
 	}
+}
+
+function requestHandler(request, response){
+	var path = url.parse(request.url).path;
+	if(path.indexOf('/led') == 0){
+		var idLed = path.split('/')[2];
+		writeToSerial(''+idLed);
+		response.statusCode = 200;
+	} else {
+		response.statusCode = 404;
+	}
+	response.end();
 }
 
 function getPortFromCommandLineParams(){
@@ -31,30 +54,42 @@ function getPortFromCommandLineParams(){
 	}
 }
 
+function onHttpServerStarted(){
+	console.log('Http server listening on port '+ httpPort);
+}
+
 function onSerialOpened (error){
 	if(error){
 		console.error('Can\'t open port '+ port +' : '+ error);
 	} else {
 		console.log('Listening on serial port '+ port);
-		serialPort.on('data', function(data) {
-			console.log('Data received: ' + data);
+		if(DEBUG){
+			serialPort.on('data', function(data) {
+				console.log('Data received: ' + data);
+			});
+		}
+	}
+}
+
+function writeToSerial (data){
+	if(serialPort.isOpen()){
+		serialPort.write(data, function(err, results){
+			if(err){
+				console.log('An error occured : '+err);
+			}
 		});
 	}
 }
 
 function onStdinLine (data){
-	if(data.toString() === 'exit'){
+	if(data.toString() === 'stop'){
 		console.log('Closing serial port '+ port +' ...');
 		serialPort.close();
 		console.log('Closing process.stdin ...');
 		rl.close();
-	}
-	if(serialPort.isOpen()){
-		serialPort.write(data.toString(), function(err, results){
-			if(err){
-				console.log('An error occured : '+err);
-			}
-		});
+		console.log('Stoping http server ...');
+		httpServer.close();
+		process.exit(0);
 	}
 }
 
