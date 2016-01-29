a0cc11a19e21baead19ff49fb72188c639e0fddc
Johan Ravry <johan.ravry@etu.univ-savoie.fr>
2016-01-29 14:35:07 +0100

    résolution des collision +1000000

--- script/effects.js
+++ script/effects.js
@@ -4,11 +4,17 @@
 });*/
 
 var keyId = 0;
+var keys = [];
+var ledWidth = $('.led-player.player-1').width();
+var ledX = $('.led-player.player-1').position().left;
+var evenenemnt = null;		
 
 	$(document).keypress(function(event){
 		switch(event.which){
 			case 49: 
 				$(".button-player.player-1").addClass('clicked');
+				if($('.led-player.player-1').hasClass('lighting')){
+				}
 				break;
 			case 50: 
 				$(".button-player.player-2").addClass('clicked');
@@ -26,9 +32,11 @@ var keyId = 0;
 				$(".button-player.player-6").addClass('clicked');
 				break;
 		}
+		evenement = event;
 	});
 
 	$(document).keyup(function(event){
+		evenement = null;
 		switch(event.which){
 			case 97: 
 				$(".button-player.player-1").removeClass('clicked');
@@ -51,43 +59,14 @@ var keyId = 0;
 		}
 	});
 
-var overlaps = (function () {
-    function getPositions( elem ) {
-        var pos, width, height;
-        pos = $( elem ).position();
-        width = $( elem ).width() / 2;
-        height = $( elem ).height();
-        return [ [ pos.left, pos.left + width ], [ pos.top, pos.top + height ] ];
-    }
-
-    function comparePositions( p1, p2 ) {
-        var r1, r2;
-        r1 = p1[0] < p2[0] ? p1 : p2;
-        r2 = p1[0] < p2[0] ? p2 : p1;
-        return r1[1] > r2[0] || r1[0] === r2[0];
-    }
-
-    return function ( a, b ) {
-        var pos1 = getPositions( a ),
-            pos2 = getPositions( b );
-        return comparePositions( pos1[0], pos2[0] ) && comparePositions( pos1[1], pos2[1] );
-    };
-})();
-
-function detectOverlapping(div1, div2) {
-    // code that detects if the box overlaps with a moving box
-   if(overlaps(div1,div2)){
-    	console.log(div1);
-    	console.log(div2);
-    }
-}
-
-
 var spawnKey = function(playerNum){
 	var keyModel = $('<div class="key key-'+keyId+'"></div>');
-
+	
 	var fatherDiv = $('.led-input.player-'+playerNum);
 	fatherDiv.append(keyModel);
+	keyModel.container = fatherDiv;
+	keyModel.player = playerNum;
+	keys.push(keyModel);
 
 	
 	var actualKey = $('.led-input.player-'+playerNum).find('.key-'+keyId);
@@ -95,89 +74,26 @@ var spawnKey = function(playerNum){
 	actualKey.animate({
 		right: '100%'
 	},{
-		duration : 5000,
+		duration : 4500,
 		easing: "linear",
-		step: function(){
-			var keyX = actualKey.position().left;
-			var ledX = correspondingLed.position().left;
-			var ledWidth = correspondingLed.width();				
-			if(keyX <=  ledX + ledWidth && keyX+15 >= ledX){
-				correspondingLed.addClass('lighting');
-				fireMakeyMakeyLeds(playerNum);
-				$(document).keypress(function(event){
-					switch(event.which){
-						case 49: 
-							if($(".led-player.player-1").hasClass('lighting')){
-								keyOnTime(playerNum, actualKey);
-								$('.led-input.player-1').toggleClass('glow',1000);
-								
-							}
-							break;
-						case 50: 
-							if($(".led-player.player-2").hasClass('lighting')){
-								keyOnTime(playerNum, actualKey);
-								$('.led-input.player-2').toggleClass('glow',1000);
-								
-							}
-							break;
-						case 51: 
-							if($(".led-player.player-3").hasClass('lighting')){
-								keyOnTime(playerNum, actualKey);
-								$('.led-input.player-3').toggleClass('glow',1000);
-								
-							}
-							break;
-						case 52: 
-							if($(".led-player.player-4").hasClass('lighting')){
-								keyOnTime(playerNum, actualKey);
-								$('.led-input.player-4').toggleClass('glow',1000);
-								
-							}
-							break;
-						case 53: 
-							if($(".led-player.player-5").hasClass('lighting')){
-								keyOnTime(playerNum, actualKey);
-								$('.led-input.player-5').toggleClass('glow',1000);
-								
-							}
-							break;
-						case 54: 
-							if($(".led-player.player-6").hasClass('lighting')){
-								keyOnTime(playerNum, actualKey);
-								$('.led-input.player-6').toggleClass('glow',1000);
-								
-							}
-							break;
-					}
-				});
-			}else{
-				$(".led-player.player-"+playerNum).removeClass('lighting');
-			}
-		},
 		complete: function(){
 
 			actualKey.stop();
 			actualKey.remove();
-			$('.led-input.player-1').find('.key').first()
+			$('.led-input.player-'+playerNum).find('.key').first()
 		}
 		});
 	keyId += 1;
 };
 
-var keyOnTime = function(playerNum, key){
+var keyOnTime = function(playerNum){
 	$(".led-player.player-"+playerNum).removeClass('lighting');
-	key.stop();
-	key.remove();
 	playKey(key);
-	
-
-
 }
-var playKey = function(){
-}
+
 
 var fireMakeyMakeyLeds = function(playerNum){
-
+	$('.led-player.player-'+playerNum).addClass('lighting');
 }
 
 var spawnRandomKey = function(){
@@ -187,3 +103,122 @@ var spawnRandomKey = function(){
 }
 
 spawnRandomKey();
+
+var collision = function(){
+	for(var key in keys){
+		var x = keys[key].position().left;
+		if(x <= ledX + ledWidth+20 && x+15 >= ledX){
+			keys[key].container.addClass('lighting');
+			if(evenement != "undefined" && evenement != null){
+				switch(evenement.which){
+						case 49: 
+							if(keys[key].player == 1){
+								keys[key].container.removeClass('lighting');
+								keys[key].remove();
+								keys.splice(key,1);
+								console.log('Player 1 SCORED !');
+							}
+							break;
+						case 50: 
+							if(keys[key].player == 2){
+								keys[key].container.removeClass('lighting');
+								keys[key].remove();
+								keys.splice(key,1);
+								console.log('Player 2 SCORED !');
+							}
+							break;
+						case 51: 
+							if(keys[key].player == 3){
+								keys[key].container.removeClass('lighting');
+								keys[key].remove();
+								keys.splice(key,1);
+								console.log('Player 3 SCORED !');
+							}
+							break;
+						case 52: 
+							if(keys[key].player == 4){
+								keys[key].container.removeClass('lighting');
+								keys[key].remove();
+								keys.splice(key,1);
+								console.log('Player 4 SCORED !');
+							}
+							break;
+						case 53: 
+							if(keys[key].player == 5){
+								keys[key].container.removeClass('lighting');
+								keys[key].remove();
+								keys.splice(key,1);
+								console.log('Player 5 SCORED !');
+							}
+							break;
+						case 54: 
+							if(keys[key].player == 6){
+								keys[key].container.removeClass('lighting');
+								keys[key].remove();
+								keys.splice(key,1);
+								console.log('Player 6 SCORED !');
+							}
+							break;
+					}
+			}
+			
+		}else if( x < ledX){
+			keys[key].container.removeClass('lighting');
+			keys[key].remove();
+			keys.splice(key,1);
+		}
+	}
+}
+
+window.setInterval(collision,10)
+
+/*step: function(){
+			var keyX = actualKey.position().left;
+			var ledX = correspondingLed.position().left;
+			var ledWidth = correspondingLed.width();				
+			if(keyX <=  ledX + ledWidth && keyX+15 >= ledX){
+				fireMakeyMakeyLeds(playerNum);
+				$(document).keypress(function(event){
+					switch(event.which){
+						case 49: 
+							if($(".led-player.player-1").hasClass('lighting')){
+								keyOnTime(playerNum, actualKey);								
+							}
+							break;
+						case 50: 
+							if($(".led-player.player-2").hasClass('lighting')){
+								keyOnTime(playerNum, actualKey);
+								
+							}
+							break;
+						case 51: 
+							if($(".led-player.player-3").hasClass('lighting')){
+								keyOnTime(playerNum, actualKey);
+
+								
+							}
+							break;
+						case 52: 
+							if($(".led-player.player-4").hasClass('lighting')){
+								keyOnTime(playerNum, actualKey);
+								
+							}
+							break;
+						case 53: 
+							if($(".led-player.player-5").hasClass('lighting')){
+								keyOnTime(playerNum, actualKey);;
+								
+							}
+							break;
+						case 54: 
+							if($(".led-player.player-6").hasClass('lighting')){
+								keyOnTime(playerNum, actualKey);
+								
+							}
+							break;
+					}
+				});
+			}else{
+				$(".led-player.player-"+playerNum).removeClass('lighting');
+			}
+		},*/
